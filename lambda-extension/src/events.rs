4cf9865a130b2eb7b0631b7df69fdcb789ed93d6
Maxime David <maxime.david@datadoghq.com>
2023-03-08 16:56:38 -0500

    fix: remove useless param in LambdaEvent

--- lambda-extension/src/events.rs
+++ lambda-extension/src/events.rs
@@ -55,16 +55,13 @@ impl NextEvent {
 /// Wrapper with information about the next
 /// event that the Lambda Runtime is going to process
 pub struct LambdaEvent {
-    /// ID assigned to this extension by the Lambda Runtime
-    pub extension_id: String,
     /// Next incoming event
     pub next: NextEvent,
 }
 
 impl LambdaEvent {
-    pub(crate) fn new(ex_id: &str, next: NextEvent) -> LambdaEvent {
+    pub(crate) fn new(next: NextEvent) -> LambdaEvent {
         LambdaEvent {
-            extension_id: ex_id.into(),
             next,
         }
     }
